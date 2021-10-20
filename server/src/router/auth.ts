96d5fe0be6a322ad57f5e329001fa337f9b1ef02
rhythm18 <rhythm.8@live.com>
2021-10-20 17:01:24 +0530

    Authentication

---
+++ server/src/router/auth.ts
@@ -0,0 +1,175 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+require('../DB/conn');
+const bcrypt = require('bcryptjs');
+const User = require('../model/users');
+const authenticate = require('../middleware/authenticate');
+const cookieParser = require("cookie-parser");
+router.use(cookieParser());
+router.get('/', (req,res) => {
+    res.send(`Hello from server auth.js`);
+});
+
+// using promises
+
+// router.post('/register', (req,res) =>{
+
+//     const {name, email, phone, designation, password, cpassword} = req.body;
+  
+//     if(!name || !email || !phone || !designation || !password || !cpassword)
+//     {
+//         return res.status(422).json({ error: "Plz fill all the fields"});
+//     }
+
+    // check for repeated email
+
+//     User.findOne({email: email})
+//         .then((userExist) =>{
+
+//             if (userExist)
+//             {
+//                 return res.status(422).json({ error: "Email already Registered"});
+//             }
+
+//             const user = new User({name, email, phone, designation, password, cpassword});
+//             user.save().then(() =>{
+//                 res.status(201).json({ message: "User Registered !"});
+//             }).catch((err) => res.status(500).json({ error: "Failed to register"}));
+//         }).catch(err => { console.log(err); });
+
+
+        
+
+// });
+
+
+router.post('/register', async (req,res) =>{
+
+    const {name, email, phone, designation, password, cpassword} = req.body;
+  
+    if(!name || !email || !phone || !designation || !password || !cpassword)
+    {
+        return res.status(422).json({ error: "Plz fill all the fields"});
+    }
+
+    // check for repeated email
+
+   try
+   {
+   const userExist = await User.findOne({email: email});
+
+    
+        if (userExist)
+        {
+            return res.status(422).json({ error: "Email already Registered"});
+        }
+        if(password != cpassword)
+        {
+            return res.status(422).json({ error: "Password and Confirm Password should be same"});
+        }
+
+        const user = new User({name, email, phone, designation, password, cpassword});
+
+        const userRegister = await user.save();
+
+        if(userRegister)
+        {
+            res.status(201).json({ message: "User Registered Successfully"});
+        }
+        else
+        {
+            res.status(500).json({ error: "Failed To Register"});
+        }
+
+
+    
+    
+
+   }
+
+   catch(err)
+
+    {
+         console.log(err); 
+    }
+
+        
+
+});
+
+router.post('/login', async (req,res) => {
+
+    let token;
+    const { email, password} = req.body;
+
+    if(!email || !password)
+    {
+        return res.status(400).json({ message: "Please fill complete data"});
+    }
+
+    try{
+
+        const userLogin = await User.findOne({email: email});
+        //console.log(userLogin);
+
+
+        if(userLogin)
+        {
+            const isMatch = await bcrypt.compare(password, userLogin.password);
+             token = await userLogin.generateAuthToken();
+            console.log(token);
+
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly:true
+            })
+            if(!isMatch)
+            {
+                return res.status(400).json({ message: "Invalid Credentials"});
+            }
+            else
+            {
+                res.json({message: "User Logged In Successfully"});
+                //console.log(userLogin);
+            }
+
+        }
+
+        else
+        {
+            return res.status(400).json({ message: "Invalid Credentials"});
+
+        }
+        
+
+
+    }
+
+    catch(err)
+    {
+        console.log(err);
+    }
+});
+
+
+
+router.get("/about", authenticate, async (req, res) => {
+    res.send(req.rootUser)
+})
+// get data 
+
+router.get("/getData", authenticate, async (req, res) => {
+    res.send(req.rootUser)
+})
+
+
+router.get("/logout", async (req, res) => {
+    console.log("logged out successfully !");
+    res.clearCookie('jwtoken');
+    //res.status(200).send('user Logged out');
+    return res.status(200).redirect('/login');
+
+})
+module.exports= router;
